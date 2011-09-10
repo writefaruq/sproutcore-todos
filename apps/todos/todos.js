5d55f440ee53cdba881e651846b46cd1b16eb894
M Omar Faruque Sarker <writefaruq@gmail.com>
2011-09-10 10:48:23 +0100

    Initial todos app

---
+++ apps/todos/todos.js
@@ -0,0 +1,49 @@
+// ==========================================================================
+// Project:   Todos
+// Copyright: @2011 My Company, Inc.
+// ==========================================================================
+/*globals Todos */
+
+Todos = SC.Application.create();
+
+Todos.Todo = SC.Object.extend({
+  title: null,
+    isDone: false
+    });
+
+// before SC.ready
+ 
+Todos.CreateTodoView = SC.TextField.extend({
+  insertNewline: function() {
+    var value = this.get('value');
+ 
+    if (value) {
+      Todos.todoListController.createTodo(value);
+      this.set('value', '');
+    }
+  }
+});
+
+Todos.MarkDoneView = SC.Checkbox.extend({
+  titleBinding: '.parentView.content.title',
+  valueBinding: '.parentView.content.isDone'
+});
+
+SC.ready(function() {
+  Todos.mainPane = SC.TemplatePane.append({
+    layerId: 'todos',
+    templateName: 'todos'
+  });
+});
+
+Todos.todoListController = SC.ArrayController.create({
+  // Initialize the array controller with an empty array.
+  content: [],
+ 
+  // Creates a new todo with the passed title, then adds it
+  // to the array.
+  createTodo: function(title) {
+    var todo = Todos.Todo.create({ title: title });
+    this.pushObject(todo);
+  }
+});
